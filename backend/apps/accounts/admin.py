a4e39341a9776e0b8ea7d3bea48c53e0f5fdf911
MohammedAMohsen <145121775+MohammedAMohsen@users.noreply.github.com>
2026-07-27 17:10:45 +0300

    Setting up Project, install infrastructure and Custom User Model

---
+++ backend/apps/accounts/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    extra = 0
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "email",
+        "username",
+        "is_staff",
+        "is_active",
+    )
+    search_fields = (
+        "email",
+        "username",
+    )
+    inlines = [ProfileInline]
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
---
+++ backend/apps/accounts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    pending_email = models.EmailField(unique=True, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    avatar = models.ImageField(default='user/avatars/default.png', upload_to="user/avatars/", blank=True)
+    cover = models.ImageField(default='user/cover/defaultCover.jpg', upload_to='user/cover/', blank=True)
+    bio = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=40, blank=True)
+    website = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    is_identity_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
